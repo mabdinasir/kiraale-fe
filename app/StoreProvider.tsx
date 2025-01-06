d5c54628d9d21d4eec695861fd662a483d78ca9d
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-01-06 16:17:13 +1100

    APP-10 RTK-Query Integration

---
+++ app/StoreProvider.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { FC, useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from '@store/store'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const StoreProvider: FC<Props> = ({ children }) => {
+  const storeRef = useRef<AppStore>(undefined)
+  if (!storeRef.current) {
+    storeRef.current = makeStore()
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>
+}
+
+export default StoreProvider
---
+++ store/middleWareArray.ts
@@ -0,0 +1,5 @@
+import { todosApi } from './api/todos'
+
+const middlewareArray = [todosApi.middleware]
+
+export default middlewareArray
