541e56cdd0b731231df14e9f7d443cdbe751ec69
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-01-08 07:01:28 +1100

    APP-11 Theme Setup (#3)
    * APP-11 Theme Setup
    
    * fmt

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
-import { Rubik } from 'next/font/google'
+import { League_Spartan } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
+import '@assets/css/materialdesignicons.min.css'
+import '@assets/css/tailwind.css'
 import '../globals.css'
 
-const rubik = Rubik({
+const league_Spartan = League_Spartan({
     subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700', '800', '900'],
     display: 'swap',
-    style: ['italic', 'normal'],
-    preload: true,
-    weight: ['500', '600', '700', '800', '900'],
+    variable: '--font-league_Spartan',
 })
 
 export const metadata: Metadata = {
@@ -30,8 +31,8 @@ export default async function RootLayout({
     const messages = await getMessages()
 
     return (
-        <html lang={locale}>
-            <body className={`${rubik.className} antialiased`}>
+        <html lang={locale} className="light scroll-smooth" dir="ltr">
+            <body className={`${league_Spartan.className} dark:bg-slate-900 antialiased`}>
                 <NextIntlClientProvider messages={messages}>
                     {children}
                     <Analytics />
