713c65a14751ea22413a789ebce43001de916720
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-03-26 17:34:50 +1100

    cors

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,27 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'http://3.25.52.143:8080',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, PUT, DELETE, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-Requested-With, Content-Type, Authorization',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 export default withNextIntl(nextConfig)
