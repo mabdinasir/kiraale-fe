7b915e8e2c38d189a07fa35f091b97cefdafd751
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-04-22 19:24:35 +1000

    update media upload and some translations

--- app/[locale]/dashboard/properties/components/PropertyMedia.tsx
+++ app/[locale]/dashboard/properties/components/PropertyMedia.tsx
@@ -140,8 +140,7 @@ const PropertyMedia = () => {
         mediaFiles.some((media) => media.file && !isValidFile(media.file)) ||
         mediaFiles.every((media) => media.id || !media.file)
 
-    const isSelectDisabled =
-        isSuccess || uploading || mediaFiles.length < 4 || mediaFiles.length > 10 || isDeletingMedia
+    const isSelectDisabled = isSuccess || uploading || mediaFiles.length > 10 || isDeletingMedia
 
     return (
         <div>
